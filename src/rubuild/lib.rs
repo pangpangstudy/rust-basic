ffd07009267decf70b5e52abba08c66de7aad6f1
Zzh <3169427572@qq.com>
2024-08-18 23:56:22 +0800

    错误处理

--- src/rubuild/lib.rs
+++ src/rubuild/lib.rs
@@ -1,12 +1,21 @@
-use std::{error::Error, fs};
+use std::{
+    env::{self, var},
+    error::Error,
+    fs,
+};
 pub struct Config {
     query: String,
     filename: String,
+    pub is_open: bool,
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("with text \n{}", contents);
-    for line in search(&config.query, &contents) {
+    let results = if config.is_open {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
         println!("{}", line);
     }
     Ok(())
@@ -20,6 +29,16 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     }
     results
 }
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
@@ -27,6 +46,11 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let is_open = env::var("IS_OPEN").is_err();
+        Ok(Config {
+            query,
+            filename,
+            is_open,
+        })
     }
 }
--- src/rubuild/main.rs
+++ src/rubuild/main.rs
@@ -7,11 +7,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     if let Err(e) = rust_basic::run(config) {
-        println!("App error: {}", e);
+        eprintln!("App error: {}", e);
         process::exit(1);
     };
 }
